cab53129ede5594dfacf625e30804f8f0de72b85
quocannguyen <78980126+quocannguyen@users.noreply.github.com>
2023-11-05 10:51:09 -0600

    Move the streamlit files outside of the folder. Change the app name to StockItUp.

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # The name of product
-st.title("StockUp")
+st.title("StockItUp")
 
-st.header("Get Ready to StockUp and Fill Your Portfolio to the Brim!")
-st.markdown("Harnessing the power of cutting-edge technology, StockUp provides a comprehensive, AI-driven approach to algorithmic earnings forecasting and stock comparison. Our platform seamlessly integrates multiple advanced tools, including yFinance API, NewsAPI, Linear Regression, and OpenAI, to deliver unparalleled insights for investors, traders, and financial enthusiasts.")
+st.header("Get Ready to StockItUp and Fill Your Portfolio to the Brim!")
+st.markdown("Harnessing the power of cutting-edge technology, StockItUp provides a comprehensive, AI-driven approach to algorithmic earnings forecasting and stock comparison. Our platform seamlessly integrates multiple advanced tools, including yFinance API, NewsAPI, Linear Regression, and OpenAI, to deliver unparalleled insights for investors, traders, and financial enthusiasts.")
 
 st.header("AI-Powered Analysis and Prediction")
 st.markdown("With our AI-powered predictive models leveraging the prowess of Linear Regression and OpenAI's sophisticated analysis, we offer robust predictions for a stock's close position, empowering you to make informed investment decisions.")
@@ -19,4 +19,4 @@ st.header("Intuitive and User-Friendly Interface")
 st.markdown("Our user-friendly interface provides an intuitive and seamless experience, allowing you to navigate through complex financial data effortlessly. Access comprehensive analytics, charts, and reports with just a few clicks, enabling you to make informed decisions with confidence.")
 
 st.header("Stay Informed, Make Educated Decisions")
-st.markdown("Empower your investment strategies with data-driven insights and expert analysis. Join StockUp today and unlock the power of AI-driven financial intelligence for more successful and informed trading decisions.")
+st.markdown("Empower your investment strategies with data-driven insights and expert analysis. Join StockItUp today and unlock the power of AI-driven financial intelligence for more successful and informed trading decisions.")
